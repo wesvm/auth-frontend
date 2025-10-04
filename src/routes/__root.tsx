b0cf395678c1183a42ddc63f55fb0c3b2fe69084
blue <samuel_walter_@hotmail.com>
2025-10-03 22:19:38 -0500

    feat: add layout and about routes with authentication check

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,8 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 const RootLayout = () => (
   <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
     <Outlet />
     <TanStackRouterDevtools />
   </>
---
+++ src/routes/_layout.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute, Link, Outlet, redirect } from '@tanstack/react-router'
+import { isAuthenticated } from '@/hooks/use-auth'
+
+export const Route = createFileRoute('/_layout')({
+  component: Layout,
+  beforeLoad: async () => {
+    if (!isAuthenticated()) {
+      throw redirect({
+        to: '/login',
+      })
+    }
+  },
+})
+
+function Layout() {
+  return (
+    <div>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+export default Layout
